5eb9b8709c7907b2fc50c06f0e9c3aab17eef81a
Shantanu Patil <shantanupatil1899@gmail.com>
2025-01-15 12:06:47 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
-// Simple authentication logic
+// auth.js
 const user = localStorage.getItem('user');
-if (!user) {
+if (!user && window.location.pathname !== '/index.html') {
     window.location.href = 'index.html';
 }
 
-function login(username) {
+function loginUser(username) {  // Renamed to avoid conflict
     localStorage.setItem('user', username);
     window.location.href = 'exam-selection.html';
 }
